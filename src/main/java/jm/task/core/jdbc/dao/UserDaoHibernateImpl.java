41a7cc937e8c002e78dd67541fd341f92f0a3ae3
Денис Сизов <you@example.com>
2025-06-12 07:28:00 +0300

    12.06.25

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -18,26 +18,32 @@ public class UserDaoHibernateImpl implements UserDao {
     private static final String DROP_TABLE_SQL = "DROP TABLE IF EXISTS users";
     private static final String TRUNCATE_TABLE_SQL = "TRUNCATE TABLE users";
 
-    private final Util util;
-
-    public UserDaoHibernateImpl() {
-        this.util = new Util();
-    }
-
     @Override
     public void createUsersTable() {
-        executeSQL(CREATE_TABLE_SQL);
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createNativeQuery(CREATE_TABLE_SQL).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating users table", e);
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        executeSQL(DROP_TABLE_SQL);
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createNativeQuery(DROP_TABLE_SQL).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException("Error dropping users table", e);
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Transaction transaction = null;
-        try (Session session = util.getSessionFactory().openSession()) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
 
             User user = new User(name, lastName, age);
@@ -56,7 +62,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void removeUserById(long id) {
         Transaction transaction = null;
-        try (Session session = util.getSessionFactory().openSession()) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
 
             User user = session.get(User.class, id);
@@ -75,7 +81,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        try (Session session = util.getSessionFactory().openSession()) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             Query<User> query = session.createQuery("FROM User", User.class);
             return query.list();
         } catch (Exception e) {
@@ -85,20 +91,12 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        executeSQL(TRUNCATE_TABLE_SQL);
-    }
-
-    private void executeSQL(String sql) {
-        Transaction transaction = null;
-        try (Session session = util.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.createNativeQuery(sql).executeUpdate();
-            transaction.commit();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createNativeQuery(TRUNCATE_TABLE_SQL).executeUpdate();
+            session.getTransaction().commit();
         } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new RuntimeException("Error executing SQL: " + sql, e);
+            throw new RuntimeException("Error cleaning users table", e);
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -17,39 +17,34 @@ public class Util {
     private static final String USERNAME = "Sizofff1984";
     private static final String PASSWORD = "V!ctoria19891906";
 
-    private final SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory;
 
-    public Util() {
-        this.sessionFactory = buildSessionFactory();
-    }
+    public static synchronized SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
 
-    private SessionFactory buildSessionFactory() {
-        try {
-            Configuration configuration = new Configuration();
-            Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
 
-            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-            settings.put(Environment.URL, URL);
-            settings.put(Environment.USER, USERNAME);
-            settings.put(Environment.PASS, PASSWORD);
-            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-            settings.put(Environment.SHOW_SQL, "true");
-            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-            settings.put(Environment.HBM2DDL_AUTO, "update");
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
 
-            configuration.setProperties(settings);
-            configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
 
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-
-            return configuration.buildSessionFactory(serviceRegistry);
-        } catch (Exception e) {
-            throw new RuntimeException("Ошибка при создании Hibernate SessionFactory", e);
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                throw new RuntimeException("Ошибка при создании Hibernate SessionFactory", e);
+            }
         }
-    }
-
-    public SessionFactory getSessionFactory() {
         return sessionFactory;
     }
 
